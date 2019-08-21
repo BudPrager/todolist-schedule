5674bcd70ad1de417fa3572e7f0dda1d13f0823d
Matt Cridland <silver.fang@hotmail.co.uk>
2019-08-21 12:49:24 -0700

    Set up store and api strucure

---
+++ src/store/index.js
@@ -0,0 +1,19 @@
+import Vue from 'vue';
+import Vuex from 'vuex';
+import actions from './actions';
+import mutations from './mutations';
+import tasks from './modules/tasks';
+
+Vue.use(Vuex);
+
+export default new Vuex.Store({
+  modules: {
+    tasks,
+  },
+  mutations: {
+    mutations,
+  },
+  actions: {
+    actions,
+  },
+});
