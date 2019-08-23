af65708cc6f97674009c9c88fa0a8a24dd12d788
Matt Cridland <silver.fang@hotmail.co.uk>
2019-08-23 14:04:20 -0700

    Setup local Netlify dev environment

--- src/api/todoist.js
+++ src/api/todoist.js
@@ -1,8 +1,12 @@
-const apiKey = 'fakeKey';
-const apiURL = 'https://fake.com';
+const apiURL = 'https://api.todoist.com/sync/v8/sync';
 
 export default {
   getItems(next) {
+    let apiKey = '<null>';
+    fetch('/.netlify/functions/todoist-api-key')
+      .then(response => response.json())
+      .then((data) => { apiKey = data.key; });
+
     const params = {
       token: apiKey,
       sync_token: '*',
